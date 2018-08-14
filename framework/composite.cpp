97f830e5afe7eaa563b5f41b462a22259a581c83
Nicola20 <nicola.lea.libera@uni-weimar.de>
2018-08-14 19:21:40 +0200

    first composite version

---
+++ framework/composite.cpp
@@ -0,0 +1,16 @@
+#include "composite.hpp"
+#include <algorithm>
+
+Composite::Composite():
+    Shape::Shape{},
+    shapes_{} {}
+
+Composite::Composite(std::string const& name):
+    Shape::Shape{name,{}},
+    shapes_{} {}
+
+void Composite::addShape(std::shared_ptr<Shape> const& shape) {
+    shapes_.push_back(shape);
+}
+
+//brauchen hier noch ne intersect Methode, weiß aber noch nicht so recht warum und wie
---
+++ framework/composite.hpp
@@ -0,0 +1,25 @@
+#ifndef COMPOSITE_HPP
+#define COMPOSITE_HPP
+
+#include "shape.hpp"
+#include "ray.hpp"
+#include <vector>
+#include <string>
+
+class Composite : public Shape {
+
+    public:
+    Composite();
+    Composite (std::string const& name);
+    //double area() const override;
+    //double volume() const override;
+    void addShape(std::shared_ptr<Shape> const& shape);
+    std::ostream& print(std::ostream& os) const override;
+    // bool intersect (Ray const& ray) override; // need this but with optimal hit instead of bool
+
+    private:
+    std::vector<std::shared_ptr<Shape>> shapes_;
+
+};
+
+#endif
