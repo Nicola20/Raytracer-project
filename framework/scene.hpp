2e60d54653bced19f89bf0e95e8177de7f868f1e
Nicola20 <nicola.lea.libera@uni-weimar.de>
2018-08-14 20:33:53 +0200

    changed scene according to needs

--- framework/scene.hpp
+++ framework/scene.hpp
@@ -5,17 +5,24 @@
 #include "sphere.hpp"
 #include "box.hpp"
 #include "color.hpp"
+#include "composite.hpp"
+#include "lightsource.hpp"
+#include "camera.hpp"
 #include <string>
 #include <vector>
 #include <map>
-#include <set>
 
 
 struct Scene {
-   /*
-    Color ia_;
+   
+    Color ia_; //Ambient
     std::map<std::string, std::shared_ptr<Material>> map_mat;
-*/
+    float width_;
+    float heigth_;
+    std::shared_ptr<Composite> composite_;
+    std::vector<Lightsource> light_;
+    Camera cam_;
+
 
 };
 #endif
