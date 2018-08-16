32b6537cd61a7fd2b69175b6a20c2f92ccca90f1
Nicola20 <nicola.lea.libera@uni-weimar.de>
2018-08-16 17:39:56 +0200

    added another constructor and ostream for testing

--- framework/camera.cpp
+++ framework/camera.cpp
@@ -1,15 +1,41 @@
 #include "camera.hpp"
 
-Camera::Camera (std::string const& name, double fox_x) :
-    upVector_{0.0f, 1.0f, 0.0f},
-    direction_{0.0f, 0.0f, -1.0f},
+Camera::Camera ():
+    name_{"default camera"},
+    fox_x_{45.0},
     eyePos_{0.0f, 0.0f, 0.0f},
-    fox_x_{fox_x},
-    name_{name} {}
+    direction_{0.0f, 0.0f, -1.0f},
+    upVector_{0.0f, 1.0f, 0.0f} {}
 
-Camera::Camera(std::string const& name, double fox_x, glm::vec3 const& eye, glm::vec3 const& dir, glm::vec3 const& up):
+
+
+Camera::Camera (std::string const& name, float fox_x):
+    name_{name},
+    fox_x_{fox_x},
+    eyePos_{0.0f, 0.0f, 0.0f},
+    direction_{0.0f, 0.0f, -1.0f},
+    upVector_{0.0f, 1.0f, 0.0f}  {}
+
+Camera::Camera(std::string const& name, float fox_x, glm::vec3 const& eye, glm::vec3 const& dir, glm::vec3 const& up):
     name_{name},
     fox_x_{fox_x},
     eyePos_{eye},
     direction_{dir},
     upVector_{up} {}
+
+
+ std::ostream& Camera::print(std::ostream& os) const {
+    os << "Camera: " << name_<<"\n" 
+        "fox_x: " << fox_x_ << 
+        "eyePosition: ( " << eyePos_.x << ", " <<
+        eyePos_.y << ", " << eyePos_.z << ") \n"
+        "Blickrichtung: (" << direction_.x << ", " <<
+        direction_.y << ", " << direction_.z << " ) \n"
+        "UpVector: () " << upVector_.x << ", " <<
+        upVector_.y << ", " << upVector_.z << " )" << "\n";
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, Camera const& cam) {
+    return cam.print(os);
+}
--- framework/camera.hpp
+++ framework/camera.hpp
@@ -3,18 +3,24 @@
 
 #include <string>
 #include <glm/vec3.hpp>
+#include <iostream>
 
 struct Camera {
 
-    Camera(std::string const& name, double fox_x);
-    Camera(std::string const& name, double fox_x, glm::vec3 const& eye, glm::vec3 const& dir, glm::vec3 const& up);
+    Camera();
+    Camera(std::string const& name, float fox_x);
+    Camera(std::string const& name, float fox_x, glm::vec3 const& eye, glm::vec3 const& dir, glm::vec3 const& up);
+    std::ostream& print (std::ostream& os) const;
+
 
     std::string name_;
-    double fox_x_;
+    float fox_x_;
     glm::vec3 eyePos_;
     glm::vec3 direction_;
     glm::vec3 upVector_;
     
 
 };
+
+std::ostream& operator<<(std::ostream& os, Camera const& cam);
 #endif
